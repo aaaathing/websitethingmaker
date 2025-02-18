d31e021fb84e2da2764d4935e15850acb3659b83
aaaathing <aarontao950@gmail.com>
2025-02-18 02:19:17 +0000

    update strangePaths.js

--- strangePaths.js
+++ strangePaths.js
@@ -1,6 +1,8 @@
-app.use(getPostTextSmall)
-app.use(async(req,res,next) => {
-	if(req.url.match(/[\p{C}`~!@$%^=+\{\}\[\]:;<>]/u)){
-		Log("suspicious: ", where(req), cap(req.username+":"+req.who.id), req.method, req.url, req.body)
+
+app.use(getPostTextSmall,async(req,res,next) => {
+	let path = decodeURI(req.url)
+	if(path.match(/[\p{C}`~!@$%^=+\{\}\[\]:;<>]/u)){
+		console.log("suspicious:", where(req), cap(req.username+":"+req.who.id), req.method, req.url, req.body ? "body:"+JSON.stringify(req.body) : "")
 	}
+	next()
 })
