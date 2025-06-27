6038af5d3c0dea468ca1bb634f880a1e6a538b42
aaaathing <aarontao950@gmail.com>
2025-06-27 19:35:43 +0000

    Fix common.js by adding back needed things

--- public/assets/common.js
+++ public/assets/common.js
@@ -68,17 +68,114 @@ document.body.style.filter = "grayscale("+(1-(userCount/50))+")"*/
 let urlParams1 = new URLSearchParams(location.search)
 var doAddNav = !urlParams1.has("nonav")
 
-let swRegister
-if(navigator.serviceWorker) swRegister = navigator.serviceWorker.register('/sw.js', {
+let luserInfo, canLogin = true
+try{
+	luserInfo = USERDATA
+}catch(e){
+	canLogin = false
+}
+const userInfo = luserInfo
+ 
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+ 
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+ 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+ 
+const publicVapidKey = 'BC97-wjdng136e_0JIJV3CHzcPKzfJsaCMscJrkoB1GMuyOJY8AvJg70WmGY5io5mPUEaBEbHrizKUvqqFagd5g';
+ 
+async function subscribe() {
+  if(!swRegister) return Swal.fire({
+    title:"Wait!",
+    text: 'Please wait for service worker to register.',
+    icon: 'error',
+  })
+  if(subscription) return Swal.fire({
+    title:"Wait!",
+    text: 'You already subscribed.',
+    icon: 'error',
+  })
+  subscription = await swRegister.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+  })
+  await fetch('/subscribe', {
+    method: 'POST',
+    body: JSON.stringify(subscription),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+async function sameSubscribe(){
+  if(!subscription) return console.error("no subscription")
+  await fetch('/subscribe', {
+    method: 'POST',
+    body: JSON.stringify(subscription),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+ 
+let swRegister, subscription
+if(navigator.serviceWorker) navigator.serviceWorker.register('/sw.js', {
   scope: '/'
+}).then(r => {
+	swRegister = r
+  windowLoadedForPush++
+  mentionNotifications()
 })
-
-let userInfo = null
 
 var script = document.createElement("script")
 script.src = "/assets/localforage.js"
 document.body.appendChild(script)
 let localforageScript = script
+
+let windowLoadedForPush = 0
+localforageScript.addEventListener("load", function(){
+  windowLoadedForPush++
+  mentionNotifications()
+});
+async function mentionNotifications(){
+  if(windowLoadedForPush !== 2) return console.log("not mention notifs "+windowLoadedForPush)
+  if(!userInfo) return
+  console.log("mention notifs")
+  if(await localforage.getItem("noNotifs")) return
+  if (await localforage.getItem("notifs")) {
+    subscription = await swRegister.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+    })
+  }else{
+    Swal.fire({
+      title:"Notifcations!",
+      text:"Please consider allowing notifications, to get notified of new things, and other notifications if you are logged in!",
+      toast: true,
+      denyButtonText:"No thank you!",
+      confirmButtonText:"Yes please!",
+      showConfirmButton:true,
+      showDenyButton:true,
+      position: 'top-right',
+    }).then(async result => {
+      if (result.isConfirmed) {
+        await localforage.setItem("notifs", "true")
+        await subscribe()
+      }else if(result.isDenied){
+        await localforage.setItem("noNotifs", "true")
+      }
+    })
+  }  
+}
 
 
 //====================NAVBAR===============
@@ -104,7 +201,6 @@ navbar.innerHTML = `
 			<a href="/minekhan/">MineKhan (thingmaker version)</a>
     </div>
   </div>
-	<a href="/mcs.html">Lake and mountains</a>
 
 	<a class="right" onclick="navbar.classList.remove('navbarStick')">&times;</a>
 
@@ -118,12 +214,10 @@ navbar.innerHTML = `
   </div>
 
 	<a class="right" href="/old.html">(2) message to people from 2021-2023</a>
-`
-document.body.prepend(navbar)
-/*
+	
 	<span id="adminNav"></span>
 
-	<span id="userNav" style="display:none;">
+	<span id="userNav" style="${canLogin?"":"display:none;"}">
   <a class="right" id="loggedIn" href="/login">Log in</a>
   <div class="dropdown right" id="usernameDropdown" style="display:none;">
     <a class="dropdown-name"></a>
@@ -134,7 +228,8 @@ document.body.prepend(navbar)
   </div>
   <a class="right" id="notifs" href="/notifs">Notifications</a>
 	</span>
-*/
+`
+document.body.prepend(navbar)
 
 var style=document.createElement("style")
 style.innerHTML = `
@@ -213,7 +308,30 @@ async function setTheme(theme){
 	updateTheme(theme)
 }
 
-//=============== Logged in is in websitecontent common.js now
+//================LOGGEDIN==============
+/*function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}*/
+ 
+function findUnread(n){
+  var a = 0
+  for(var i=0; i<n.length; i++){
+    if(!n[i].read) a++
+  }
+  if(a > 0) return a
+}
 
 function addBanner(text, bg = "white", color = "black"){
   var div = document.createElement("div")
@@ -226,6 +344,43 @@ function addBanner(text, bg = "white", color = "black"){
   document.body.prepend(div)
 }
 
+var loggedInEl = document.getElementById("loggedIn")
+var notifs = document.getElementById("notifs")
+notifs.style.display = "none"
+var logged = userInfo && userInfo.username
+if(loggedInEl && logged){
+  var usernameEl = document.querySelector("#usernameDropdown .dropdown-name")
+  if(usernameEl){
+    loggedInEl.style.display = "none"
+    document.getElementById("usernameDropdown").style.display = ""
+    usernameEl.innerHTML = logged
+    usernameEl.href = "/account"
+    document.querySelector("#usernameDropdown-profile").href="/user?user="+escape(logged)
+  }else{
+    loggedInEl.innerHTML = logged
+    loggedInEl.href = "/account"
+  }
+  notifs.style.display = ""
+  if(userInfo.notifs){
+    var amount = findUnread(userInfo.notifs)
+    notifs.innerHTML += amount ? (" ("+amount+")") : ""
+  }
+  if(userInfo.admin){
+    document.querySelector("#adminNav").innerHTML = `
+<a href="/admin/users.html">Users</a>
+<a href="/admin/log.html">Log</a>
+`
+  }
+}
+/*.catch(function(e){
+  console.log(e)
+  addBanner("Something went wrong when fetching","var(--red)")
+})*/
+/*
+var logged = getCookie("username")
+if(logged){
+  loggedInEl.innerHTML = logged
+}*/
 var script = document.createElement("script")
 script.src = "/news.js"
 document.body.appendChild(script)
@@ -383,6 +538,107 @@ function hashCode(str) {
   return hash;
 }
 
+function enableUserPopup(el,user){
+  var hoveringEl = false, hoveringPopup = false
+  el.addEventListener("mouseover", function(e){
+    hoveringEl = true
+    var popup = el.previousElementSibling
+    if(popup && popup.classList.contains("popup")) return
+    popup = document.createElement("div")
+    popup.className = "popup"
+    var popupContent = document.createElement("span")
+    popup.appendChild(popupContent)
+    popupContent.innerHTML = `<h3 class="skeletonText" style="width:200px;">&nbsp;</h3><br><span class="skeletonText" style="width:300px;">&nbsp;</span><br><span class="skeletonText" style="width:100px;">&nbsp;</span>`
+    el.parentNode.insertBefore(popup, el);
+    popup.addEventListener("mouseover", function(e){
+      hoveringPopup = true
+    })
+    popup.addEventListener("mouseout", function(e){
+      hoveringPopup = false
+      setTimeout(function(){
+        if(!hoveringPopup && !hoveringEl){
+          popup.remove()
+        }
+      },1000)
+    })
+    fetch(`/server/account/${user}`).then(r => r.json()).then(r => {
+      if(!r){
+        return popupContent.innerHTML = "User doesn't exist: "+user
+      }
+      popupContent.innerHTML = `${r.bg ? '<div class="bg"></div>' : ''}
+      <div class="userContent">
+      <a href="/user?user=${r.username}"><h3 style="display:inline-block;">
+      <img class="pfp" style="width:30px;height:30px;border-radius:100%;border:1px solid gray;vertical-align:middle;">
+      ${r.username}</h3></a><div class='popupVotes' style="margin-left:16px;display:inline-block;"></div><br>
+      ${r.bio ? ""+r.username+" - "+format(r.bio) : ""}<br>
+      ${r.lastActive ? "Last active: "+timeString(r.lastActive) : ""}</div>`
+      if(r.bg) popupContent.querySelector(".bg").style.backgroundImage = 'url('+r.bg+')'
+      popupContent.querySelector(".pfp").src = r.pfp
+      makeVotes(popupContent.querySelector(".popupVotes"),r, userInfo && userInfo.username)
+    })
+  })
+  el.addEventListener("mouseout", function(e){
+    hoveringEl = false
+    var popup = el.previousElementSibling
+    if(!popup || !popup.classList.contains("popup")) return
+    setTimeout(function(){
+      if(!hoveringEl && !hoveringPopup){
+        popup.remove()
+        hoveringPopup = false
+      }
+    },1000)
+  })
+}
+ 
+function makeVotes(el,data,yourUsername){
+  let username = data.username
+  el.innerHTML = `<div class="popupContainer">
+<a class="allVotes"></a>
+<div class="popup">
+	<span style="padding:8px;">
+		<button class="vote_1 small">+1</button>
+		<button class="vote_0 small">0</button>
+		<button class="vote_-1 small">-1</button>
+		<br><br>
+		<span class="voteInfo"></span>
+		<br><br>
+		<span style="color:gray;">If a user has 10 or more votes and 70% or more positive, they can create and edit wiki pages and delete some things.</span>
+	</span>
+</div>
+</div>`
+  let voteEl = el.querySelector(".allVotes")
+  let voteInfo = el.querySelector(".voteInfo")
+	let yourVote = data.yourVote || 0
+	let votePercent, voteCount
+  function updateVotes(data){
+		({votePercent,voteCount} = data)
+    voteEl.innerHTML = Math.round(votePercent*voteCount) // + (votes >= 10 ? " ✅" : "")
+    voteEl.style.color = data.enoughVotes ? "green" : (votePercent < 0 ? "red" : "")
+		voteInfo.textContent = Math.round(votePercent*100)+"% positive | "+voteCount+" voted"
+		voteInfo.style.color = voteEl.style.color
+  }
+  updateVotes(data)
+  el.querySelector(".vote_"+yourVote).classList.add("selected")
+  function vote(amount){
+    fetch("/server/voteUser/"+username, {
+      credentials:'include',
+      method: 'POST',
+      body: JSON.stringify({vote:amount})
+    }).then(d => d.json()).then(d => {
+      if(d.success){
+        el.querySelector(".vote_"+yourVote).classList.remove("selected")
+				yourVote = amount
+        updateVotes(d)
+        el.querySelector(".vote_"+yourVote).classList.add("selected")
+      }else{
+        alert(d.message)
+      }
+    })
+  }
+  el.querySelector(".vote_1").onclick = () => vote(1)
+  el.querySelector(".vote_0").onclick = () => vote(0)
+  el.querySelector(".vote_-1").onclick = () => vote(-1)
+}
 
 /*function formatGetAttributesInString(str){
   var arr = []
